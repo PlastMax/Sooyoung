e01b81857424182bb0c9c31a995270b353fff7a2
PlastMax <limaxchen@gmail.com>
2021-07-25 03:23:50 +0200

    Sooyoung V.1

---
+++ main.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('joy ♡ is online!');
+});
+
+
+
+client.login('NDg3MDQ3NTM2OTEwMDA4MzMw.W5BsXQ.PpVgDQcEVkVVO5QrpbqH8FJ98SE');
